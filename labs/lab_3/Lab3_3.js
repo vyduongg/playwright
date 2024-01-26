b8fc150ba60cd821ac62b4c043bb39e73ab71659
loup <38435755+Forture128@users.noreply.github.com>
2024-01-26 13:15:17 +0700

    Lab 3.3

--- labs/lab_3/Lab3_3.js
+++ labs/lab_3/Lab3_3.js
@@ -4,6 +4,42 @@
 	Expected output: [1, 12, 16, 28, 34]
  */
 
+/**
+	unsortedPosition = arr.length -1
+	currentIndex = 0 && currentIndex < unsortedPosition
 
-	
-	 
+	while(unsortedPosition > 0)
+
+	Loop
+	if(leftValue > rightValue) 
+		swap
+	unsortedPosition-- ;
+ */
+
+let array = [12, 34, 1, 16, 28];
+let unsortedPosition = array.length -1;
+while(unsortedPosition > 0){
+	for (let index = 0; index < unsortedPosition; index++) {
+		let leftValue = array[index];
+		let rightValue = array[index + 1];
+
+		if (leftValue > rightValue) {
+			// Swap with temporary variable
+			let temp = leftValue;
+			array[index] = rightValue;
+			array[index+1] = leftValue;
+
+			// Swap without temporary variable
+			/**
+			 * a = 10
+			 * b = 2
+			 * a = a + b = 10 + 2 = 12 (a=12, b=2)
+			 * b = a - b = 12 - 2 = 10 (a=12, b=10)
+			 * a = a - b = 12 - 10 = 2 (a=2 , b=10)
+			 */
+		}
+	}
+	unsortedPosition--;
+}
+
+console.log(array);
