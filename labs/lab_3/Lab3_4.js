ab2e6d2fad706eff4a8a397e353b864aa01d1437
loup <38435755+Forture128@users.noreply.github.com>
2024-01-31 11:33:46 +0700

    Update lab 3.4

--- labs/lab_3/Lab3_4.js
+++ labs/lab_3/Lab3_4.js
@@ -4,7 +4,7 @@
  * Array 02: [1, 13, 16, 27, 99]
  */
 
-const arr1 = [1, 12, 16, 28, 34]; 
+const arr1 = [1, 12, 16, 28, 34];
 const arr2 = [1, 13, 16, 27, 99];
 const mergedArr = [];
 
@@ -12,32 +12,27 @@ const n1 = arr1.length;
 const n2 = arr2.length;
 let i = 0;
 let j = 0;
-let mergedPosition = 0;
 
-while(i < n1 && j < n2){
-
-	if(arr1[i] < arr2[j]){
-		mergedArr[mergedPosition] = arr1[i];
-		i++;
-	} else{
-		mergedArr[mergedPosition] = arr2[j];
-		j++
-	}
-	mergedPosition++;
+while (i < n1 && j < n2) {
+  if (arr1[i] < arr2[j]) {
+    mergedArr.push(arr1[i]);
+    i++;
+  } else {
+    mergedArr.push(arr2[j]);
+    j++;
+  }
 }
 
-while(i < n1){
-	mergedArr[mergedPosition] = arr1[i];
-	i++;
-	mergedPosition++;
+while (i < n1) {
+  mergedArr.push(arr1[i]);
+  i++;
 }
 
-while(j < n2){
-	mergedArr[mergedPosition] = arr2[j];
-	j++;
-	mergedPosition++
+while (j < n2) {
+  mergedArr.push(arr2[j]);
+  j++;
 }
 
-console.log('arr1: ', arr1);
-console.log('arr2: ', arr2);
-console.log('merged: ', mergedArr);
+console.log("arr1: ", arr1);
+console.log("arr2: ", arr2);
+console.log("merged: ", mergedArr);
